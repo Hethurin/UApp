f17a70980f1964e40a22fad5e54f4cafcdcf9d52
Hethurin <alexey.feshchenko@gmail.com>
2014-08-31 14:52:05 +0300

    Define init function. Make variables actually global

--- useless_passport_validator/ulibrary.py
+++ useless_passport_validator/ulibrary.py
@@ -2,19 +2,28 @@
 from collections import namedtuple
 
 
-"""Document constants"""
-countries = ["Mordor", "Gondor", "Lorien", "Shire"]
-genders = ["Male", "Female"]
-cities = {
-    'Mordor': 'Minas Morgul,Barad Dur',
-    'Gondor': 'Minas Tirith,Isengard,Osgiliath',
-    'Lorien': 'Lorien',
-    'Shire': 'Hobbiton,Waymeet,Frogmorton,Tuckborough'
+def init():
+    """Document constants"""
+    global countries
+    countries = ["Mordor", "Gondor", "Lorien", "Shire"]
+    global genders
+    genders = ["Male", "Female"]
+    global cities
+    cities = {
+        'Mordor': 'Minas Morgul,Barad Dur',
+        'Gondor': 'Minas Tirith,Isengard,Osgiliath',
+        'Lorien': 'Lorien',
+        'Shire': 'Hobbiton,Waymeet,Frogmorton,Tuckborough'
     }
-purpose = ["Visit", "Transit", "Work", "Immigrate"]
+    global purpose
+    purpose = ["Visit", "Transit", "Work", "Immigrate"]
 
-"""Store user input here"""
-UPassport = namedtuple("UPassport", "country name gender isscity expdate serial")
-UPass = namedtuple("UPass", "name gender purpose duration serial expires")
-UWorkVisa = namedtuple("UWorkVisa", "name proff duration expires")
-URecord = namedtuple("URecord", "purpose duration")
+    """Store user input here"""
+    global UPassport
+    UPassport = namedtuple("UPassport", "country name gender isscity expdate serial")
+    global UPass
+    UPass = namedtuple("UPass", "name gender purpose duration serial expires")
+    global UWorkVisa
+    UWorkVisa = namedtuple("UWorkVisa", "name proff duration expires")
+    global URecord
+    URecord = namedtuple("URecord", "purpose duration")
