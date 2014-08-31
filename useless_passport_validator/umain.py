a32b4216b98fb2bb23d8a0a700d05bd417df1906
Hethurin <alexey.feshchenko@gmail.com>
2014-08-31 15:09:09 +0300

    Implement main function

---
+++ useless_passport_validator/umain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.4
+import ulibrary
+import sys
+from uwindow import UWindow
+from gi.repository import Gtk
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main():
+    ulibrary.init()
+
+    uwindow = UWindow()
+    uwindow.connect("delete-event", Gtk.main.quit)
+    uwindow.show_all()
+    Gtk.main()
+
+if __name__ == "__main__":
+    sys.exit(main())
